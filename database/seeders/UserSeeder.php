ec2eae7132df92a685da8c34f84cff49af47a81b
luisozuna <lgozuna@gmail.com>
2025-03-26 23:37:40 -0600

    "Actualizar UserSeeder: cambiar ID a UUID y actualizar campos"

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,12 +12,12 @@ class UserSeeder extends Seeder
     {
         User::upsert([
             [
-                'id' => 1,
+                'id' => '9e84605e-07cd-4e8a-9c43-7170e315a5b0',
                 'role_id' => RoleEnum::Root->value,
                 'name' => env('ROOT_USER_NAME'),
                 'email' => env('ROOT_USER_EMAIL'),
                 'password' => bcrypt(env('ROOT_USER_PASSWORD')),
             ],
-        ], ['id'], ['name', 'role_id', 'email', 'password']);
+        ], ['id'], ['email', 'name', 'role_id', 'password']);
     }
 }
