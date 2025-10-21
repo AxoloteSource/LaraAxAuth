57f5ffc84275e50fcb488b0281746bbdd5995d45
Diego Silva <sfadiego@gmail.com>
2025-10-20 21:36:50 -0600

    update action data
    update action data

--- app/Data/Actions/IndexActionData.php
+++ app/Data/Actions/IndexActionData.php
@@ -2,14 +2,14 @@
 
 namespace App\Data\Actions;
 
+use App\Core\Data\IndexData;
 use Spatie\LaravelData\Attributes\Validation\Rule;
 use Spatie\LaravelData\Attributes\FromRouteParameter;
-use Spatie\LaravelData\Data;
 
-class IndexActionData extends Data
+class IndexActionData extends IndexData
 {
     public function __construct(
         #[FromRouteParameter('id'), Rule(['required', 'exists:roles,id'])]
         public string $id,
-    ) {}
+    ) {   }
 }
