d9e0e9b89d54b0b3e2a875a8d80ba256eebf3fc3
luisozuna <lgozuna@gmail.com>
2025-12-24 20:56:07 -0600

    "Agrega rol Messages, actualiza descripciones en RoleSeeder y tests"

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,13 +15,19 @@ class RoleSeeder extends Seeder
                 'id' => RoleEnum::Root->value,
                 'name' => 'Root',
                 'key' => 'root',
-                'description' => 'User root',
+                'description' => 'Super administrator with unrestricted access to all system features and actions',
             ],
             [
                 'id' => RoleEnum::Admin->value,
                 'name' => 'Admin',
                 'key' => 'admin',
-                'description' => 'User admin',
+                'description' => 'Administrator with access to manage system, settings and users',
+            ],
+            [
+                'id' => RoleEnum::Messages->value,
+                'name' => 'Messages',
+                'key' => 'messages',
+                'description' => 'User has total access to messages',
             ],
         ], ['id'], ['name', 'description']);
     }
